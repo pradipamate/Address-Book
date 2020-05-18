6bfab771c7fbf4d8cb5dc070a068182b5d3b7134
5377 <pradipamate07@gmail.com>
2020-05-19 01:53:09 +0530

    Updates

---
+++ precache-manifest.9308bbd3883f92df8baae0ada93a7d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67cd559f4c3fea00097a",
+    "url": "/Address-Book/static/css/main.679166f3.chunk.css"
+  },
+  {
+    "revision": "67cd559f4c3fea00097a",
+    "url": "/Address-Book/static/js/main.51b7033d.chunk.js"
+  },
+  {
+    "revision": "f106e8427779ecf09b82",
+    "url": "/Address-Book/static/js/runtime~main.3fd18c2c.js"
+  },
+  {
+    "revision": "c38fa088f40187472989",
+    "url": "/Address-Book/static/js/2.c16439e3.chunk.js"
+  },
+  {
+    "revision": "392530fa3d9477d7a68a61f10bdad4a3",
+    "url": "/Address-Book/index.html"
+  }
+];
